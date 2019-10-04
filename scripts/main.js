cad76e33178dbf858010859646e10916a5b3b0ad
Divlo <contact@divlo.fr>
2019-10-04 23:53:26 +0200

    Optimized DOM Manipulation

--- scripts/main.js
+++ scripts/main.js
@@ -143,9 +143,11 @@ $(function () {
     /* Permet d'afficher la liste des citations/proverbes */
     if(chemin === "/views/quote-list.php") {
         window.onload = $('.totalLengthQuote').html('Total de ' + quotes.length + ' citations.');
+        let resultat = "";
         for (index in quotes) {
-            $( ".quote-list" ).append('<tr> <td class="quote-element-list important">' + quotes[index]["source"] + '</td> <td class="quote-element-list">" ' + quotes[index]["quote"] + ' "</td> </tr>');
+            resultat = resultat + `<tr> <td class="quote-element-list important">${quotes[index]["source"]}</td> <td class="quote-element-list">${quotes[index]["quote"]}</td> </tr>`;
         }
+        $( ".quote-list" ).append(resultat);
     }
 
     $("#submitConvertCurrency").click(function() 
