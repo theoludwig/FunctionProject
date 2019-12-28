0b6e2f9181ec25369709c15320ce832826b84a54
Divlo <contact@divlo.fr>
2019-12-28 21:33:09 +0100

    Hotfix: Sticky Footer

--- incl/footer.php
+++ incl/footer.php
@@ -1,8 +1,13 @@
         <footer class="text-center">
-            <a href="#" class="scroll-top-arrow important"><i class="fas fa-arrow-up"></i></a>
-            <div class="container">
-                <div class="realDateTime">Affichage de l'heure en temps réel : <span class="important" id="realDateTime"></span></div>
-                <div class="footer-text"><a href="https://divlo.fr/" target="_blank">Divlo</a> | Tous droits réservés</div>
+            <div class="container-fluid">
+                <!-- <a href="#" class="scroll-top-arrow important"><i class="fas fa-arrow-up"></i></a> -->
+                <div class="row">
+                    <div class="col-12">
+                        <a href="#" class="scroll-top-arrow important"><i class="fas fa-arrow-up"></i></a>
+                        <div class="realDateTime">Affichage de l'heure en temps réel : <span class="important" id="realDateTime"></span></div>
+                        <div class="footer-text"><a href="https://divlo.fr/" target="_blank">Divlo</a> | Tous droits réservés</div>
+                    </div>
+                </div>
             </div>
         </footer>
 
