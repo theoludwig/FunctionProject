f6318738936916557c9405e055580653005cc592
Divlo <contact@divlo.fr>
2020-03-17 15:34:26 +0100

    frontend: Structure fichier + Header et navigation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,34 @@
 /* Libraries Imports */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 /* Components Imports */
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 /* Pages Imports */
+import Home from './pages/Home/Home';
 
 /* Contexts Imports */
 
 /* CSS Imports */
+import 'typeface-montserrat';
+import 'normalize.css';
+import './assets/css/grid.css';
+import './assets/css/general.css';
 
 function App() {
     return (
-        <p>Hello world!</p>
+        <BrowserRouter>
+            <Header />
+                <div className="content container-fluid">
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                    </Switch>
+                </div>
+            <Footer />
+        </BrowserRouter>
     );
 }
 
