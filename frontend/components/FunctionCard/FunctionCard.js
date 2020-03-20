cb3ca25aac2706dc4195e2a2826e88da18082acc
Divlo <contact@divlo.fr>
2020-03-20 18:36:48 +0100

    frontend: FunctionCard CSS Function__info bottom

--- frontend/components/FunctionCard/FunctionCard.js
+++ frontend/components/FunctionCard/FunctionCard.js
@@ -4,9 +4,11 @@ import './FunctionCard.css';
 const FunctionCard = (props) =>  (
     <Link href={`/functions/${props.slug}`}>
         <div className="FunctionCard col-sm-24 col-md-10 col-xl-7">
-            <img className="FunctionCard__image" alt={props.title} src={props.image} />
-            <h2 className="FunctionCard__title">{props.title}</h2>
-            <p className="FunctionCard__description">{props.description}</p>
+            <div className="FunctionCard__top">
+                <img className="FunctionCard__image" alt={props.title} src={props.image} />
+                <h2 className="FunctionCard__title">{props.title}</h2>
+                <p className="FunctionCard__description">{props.description}</p>
+            </div>
             <div className="FunctionCard__info">
                 <p className="FunctionCard__category" style={{ backgroundColor: props.category.color }}>{props.category.name}</p>
                 <p className="FunctionCard__publication-date">{props.publicationDate}</p>
