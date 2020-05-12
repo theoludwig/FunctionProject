45df3a6851d4aef2fd49c043d17540586b76a4d1
Divlo <contact@divlo.fr>
2020-05-13 00:23:43 +0200

    👌 IMPROVE: Custom express server - NextJS website

--- website/server.js
+++ website/server.js
@@ -1,28 +1,28 @@
-const { createServer } = require("http");
-const { parse }        = require("url");
-const next             = require("next");
+/* Modules */
+const next            = require('next');
+const express         = require("express");
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 
+/* Variables */
 const PORT   = process.env.PORT || 3000;
 const dev    = process.env.NODE_ENV !== "production";
 const app    = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-    createServer((req, res) => {
-        // Be sure to pass `true` as the second argument to `url.parse`.
-        // This tells it to parse the query portion of the URL.
-        const parsedUrl = parse(req.url, true);
-        const { pathname, query } = parsedUrl;
+    const server = express();
 
-        if (pathname === "/a") {
-            app.render(req, res, "/b", query);
-        } else if (pathname === "/b") {
-            app.render(req, res, "/a", query);
-        } else {
-            handle(req, res, parsedUrl);
-        }
-    }).listen(PORT, (err) => {
-        if (err) throw err;
+    /* Middlewares */
+    server.use(redirectToHTTPS([/localhost:(\d{4})/]));
+
+    /* Routes */
+    server.all("*", (req, res) => {
+        return handle(req, res);
+    });
+
+    /* Server */
+    server.listen(PORT, (error) => {
+        if (error) throw error;
         console.log(`> Ready on http://localhost:${PORT}`);
     });
 });
