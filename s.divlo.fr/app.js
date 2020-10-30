4efd702ca3e433a847de8bb9a0c0a3e416ccbe7a
divlo <contact@divlo.fr>
2020-10-30 22:37:04 +0100

    fix: s.divlo.fr database password environment

--- s.divlo.fr/app.js
+++ s.divlo.fr/app.js
@@ -12,7 +12,7 @@ const app = express()
 const database = mysql.createPool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASS,
+  password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   port: process.env.DATABASE_PORT
 })
